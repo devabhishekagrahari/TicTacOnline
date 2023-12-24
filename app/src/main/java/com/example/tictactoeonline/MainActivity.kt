53250d50f506e8278f02609109e0d8fba00fc960
Abhishek Agrahari <devabhishekagrahari@gmail.com>
2023-12-24 16:47:14 +0530

    TicTacOnline V 2.0

--- app/src/main/java/com/example/tictactoeonline/MainActivity.kt
+++ app/src/main/java/com/example/tictactoeonline/MainActivity.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tictactoeonline.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +22,13 @@ class MainActivity : AppCompatActivity() {
         binding.playOfflineBtn.setOnClickListener {
               createOfflineGame()
         }
+        binding.createOnlineGameBtn.setOnClickListener {
+            createOnlineGame()
+        }
+
+        binding.joinOnlineGameBtn.setOnClickListener {
+            joinOnlineGame()
+        }
     }
      fun createOfflineGame(){
          GameData.saveGameMOdel(
@@ -27,6 +39,39 @@ class MainActivity : AppCompatActivity() {
          startGame()
 
      }
+    fun createOnlineGame(){
+        GameData.myID ="X"
+        GameData.saveGameMOdel(
+            GameModel(
+                gameStatus = GameStatus.CREATED,
+                gameId = Random.nextInt(1000 .. 9999).toString()
+            )
+        )
+        startGame()
+    }
+
+    fun joinOnlineGame(){
+     var gameId = binding.gameIdInput.text.toString()
+        if(gameId.isEmpty()){
+            binding.gameIdInput.setError("Please Enter Game ID")
+            return
+        }
+        GameData.myID ="O"
+        Firebase.firestore.collection("games")
+            .document(gameId)
+            .get()
+            .addOnSuccessListener {
+                val model =it?.toObject(GameModel::class.java)
+                if (model==null){
+                    binding.gameIdInput.setError("ase enter valid game ID")
+                }else{
+                    model.gameStatus = GameStatus.JOINED
+                    GameData.saveGameMOdel(model)
+                    startGame()
+                }
+            }
+        }
+
     fun startGame(){
        startActivity(Intent(this,GameActivity::class.java))
     }
